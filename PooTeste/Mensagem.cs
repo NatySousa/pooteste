dad3ad7bf20aa9d3e31308201122341475ecea2c
NatySousa <nataliaspsousa@gmail.com>
2021-08-23 10:50:18 -0300

    Versão inicial

---
+++ PooTeste/Mensagem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooTeste
+{
+    public class Mensagem
+    {
+        private string textoMensagem;
+        public string TextoMensagem
+        {
+            get
+            {
+                return this.textoMensagem;
+            }
+
+            set
+            {
+                this.textoMensagem = value.ToUpper();
+            }
+        }
+
+        public void ExibirMensagem()
+        {
+            Console.WriteLine(this.TextoMensagem);
+        }
+
+    }
+}
---
+++ PooTeste/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mensagem msg1;
+            msg1 = new Mensagem();
+            msg1.TextoMensagem = "Alô Mundo!";
+            msg1.ExibirMensagem();
+            Console.ReadKey();      
+        }
+    }
+}
